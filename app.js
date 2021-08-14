b627f05e7416614424f46e6c84f0f288aecd7e71
Vaskar Dhakal <vdhakal10@gmail.com>
2021-08-14 14:03:15 -0500

    mongodb conncetion

--- app.js
+++ app.js
@@ -6,17 +6,13 @@ const cors = require('cors');
 
 const app = express();
 
-// allow cross-origin requests
 app.use(cors());
 
-// connect to mlab database
-// make sure to replace my db string & creds with your own
 mongoose.connect('mongodb://ninja:test@ds161148.mlab.com:61148/graphql-ninja')
 mongoose.connection.once('open', () => {
     console.log('conneted to database');
 });
 
-// bind express with graphql
 app.use('/graphql', graphqlHTTP({
     schema,
     graphiql: true
